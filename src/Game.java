f172bc5819cc28a97c5b56e876e6c116f66bfc83
Biszku <daniel.bivhuniak@gmail.com>
2024-06-28 18:31:12 +0200

    Si under work

--- src/Game.java
+++ src/Game.java
@@ -1,25 +1,35 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Game {
 
-//    char player;
-    private TicTacToeBoard board;
+    private char realPlayer;
+    private BoardController board;
     private char currentPlayer;
 
     public Game() {
         Random random = new Random();
-//        player = random.nextInt(1,3) == 1 ? 'x' : 'o';
-        board = new TicTacToeBoard();
+        realPlayer = random.nextInt(1,3) == 1 ? 'x' : 'o';
+        board = new BoardController();
         currentPlayer = 'x';
+    }
+
+    public void showAllBoards() {
+//        System.out.println(board.getAllBoard().size());
+
+for(TicTacToeBoard board : board.getAllBoard()) {
+            board.displayCurrentBoard();
+        }
     }
 
     public void start() {
         board.displayCurrentBoard();
 
         while(true) {
-            board.enterMove(currentPlayer);
+            if(currentPlayer == realPlayer) board.enterMove(currentPlayer, board.getBoard());
+            else board.enterMove(currentPlayer, board.getBoard());
             board.displayCurrentBoard();
-            boolean isWinner = board.checkWinner(currentPlayer, board.getBoard());
+            boolean isWinner = checkWinner(currentPlayer, board.getBoard());
             if(isWinner) break;
             changePlayer();
         }
@@ -29,5 +39,27 @@ public class Game {
 
     public void changePlayer() {
         currentPlayer = currentPlayer == 'x' ? 'o' : 'x';
+    }
+
+    public boolean checkWinner(char player, char[][] board) {
+        char[] win = new char[] {player, player, player};
+
+        // horizontal checking
+        for(char[] tab : board) {
+            if(Arrays.equals(tab,win)) return true;
+        }
+
+        // vertical checking
+        for(int i = 0; i < board.length; i++) {
+            char[] newArr = new char[] {board[0][i],board[1][i],board[2][i]};
+            if(Arrays.equals(newArr,win)) return true;
+        }
+
+        // cross checking
+        char[] cross1 = new char[] {board[0][0],board[1][1],board[2][2]};
+        char[] cross2 = new char[] {board[0][2],board[1][1],board[2][0]};
+        if(Arrays.equals(cross1,win) || Arrays.equals(cross2,win)) return true;
+
+        return false;
     }
 }
