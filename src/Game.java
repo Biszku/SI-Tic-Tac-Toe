87f40824576934d0c6cab39b2d387b0d96f3b88b
Biszku <daniel.bivhuniak@gmail.com>
2024-06-28 20:49:02 +0200

    Added draw handling

--- src/Game.java
+++ src/Game.java
@@ -14,14 +14,6 @@ public class Game {
         currentPlayer = 'x';
     }
 
-    public void showAllBoards() {
-//        System.out.println(board.getAllBoard().size());
-
-for(TicTacToeBoard board : board.getAllBoard()) {
-            board.displayCurrentBoard();
-        }
-    }
-
     public void start() {
         board.displayCurrentBoard();
 
@@ -29,8 +21,12 @@ for(TicTacToeBoard board : board.getAllBoard()) {
             if(currentPlayer == realPlayer) board.enterMove(currentPlayer, board.getBoard());
             else board.enterMove(currentPlayer, board.getBoard());
             board.displayCurrentBoard();
-            boolean isWinner = checkWinner(currentPlayer, board.getBoard());
+            boolean isWinner = board.checkWinner(currentPlayer, board.getBoard());
             if(isWinner) break;
+            if(board.isFull(board.getBoard())) {
+                System.out.println("This is draw");
+                return;
+            };
             changePlayer();
         }
 
@@ -39,27 +35,5 @@ for(TicTacToeBoard board : board.getAllBoard()) {
 
     public void changePlayer() {
         currentPlayer = currentPlayer == 'x' ? 'o' : 'x';
-    }
-
-    public boolean checkWinner(char player, char[][] board) {
-        char[] win = new char[] {player, player, player};
-
-        // horizontal checking
-        for(char[] tab : board) {
-            if(Arrays.equals(tab,win)) return true;
-        }
-
-        // vertical checking
-        for(int i = 0; i < board.length; i++) {
-            char[] newArr = new char[] {board[0][i],board[1][i],board[2][i]};
-            if(Arrays.equals(newArr,win)) return true;
-        }
-
-        // cross checking
-        char[] cross1 = new char[] {board[0][0],board[1][1],board[2][2]};
-        char[] cross2 = new char[] {board[0][2],board[1][1],board[2][0]};
-        if(Arrays.equals(cross1,win) || Arrays.equals(cross2,win)) return true;
-
-        return false;
     }
 }
